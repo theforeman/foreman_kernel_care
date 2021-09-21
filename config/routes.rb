48b29815e16a86aba9ccf3e5850cd28a0def0a0c
maccelf <maxmol27@gmail.com>
2021-09-21 11:40:19 +0300

    Override import tracer profile method
    Now method checks if the kernelcare package is installed and removes the 
    trace if it is a kernel application

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-ForemanPluginTemplate::Engine.routes.draw do
+ForemanKernelCare::Engine.routes.draw do
   get 'new_action', to: 'example#new_action', as: 'new_action'
   get 'plugin_template_description', to: 'example#react_template_page_description'
   get 'welcome', to: '/react#index', as: 'welcome'
 end
 
 Foreman::Application.routes.draw do
-  mount ForemanPluginTemplate::Engine, at: '/foreman_kernel_care'
+  mount ForemanKernelCare::Engine, at: '/foreman_kernel_care'
 end
