0d235261c009310b1e6da871ff00af7fd7128e3c
maccelf <maxmol27@gmail.com>
2021-10-13 15:48:22 +0300

    Add tests (#4)

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -2,5 +2,8 @@
 require 'test_helper'
 
 # Add plugin to FactoryBot's paths
-FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
+rex_factories_path = "#{ForemanRemoteExecution::Engine.root}/test/factories"
+plugin_factories_path = File.join(File.dirname(__FILE__), 'factories')
+FactoryBot.definition_file_paths << rex_factories_path
+FactoryBot.definition_file_paths << plugin_factories_path
 FactoryBot.reload
