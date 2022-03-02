00e5d5bad3190df62855e86bb8aa3455680f2e8b
Maxim Petukhov <maxmol27@gmail.com>
2022-03-02 19:51:26 +0300

    Add kernel patched version
    Add new template to run the command `kcare-uname -r`
    Add new seeds for the template

--- db/seeds.d/100_assign_features_with_templates.rb
+++ db/seeds.d/100_assign_features_with_templates.rb
@@ -16,6 +16,13 @@ User.as_anonymous_admin do
         module_template.organizations << Organization.unscoped.all if module_template.organizations.empty?
         module_template.locations << Location.unscoped.all if module_template.locations.empty?
       end
+
+      module_template = JobTemplate.find_by(name: 'Kernel version')
+      if module_template && !Rails.env.test? && Setting[:remote_execution_sync_templates]
+        module_template.sync_feature('kernel_version')
+        module_template.organizations << Organization.unscoped.all if module_template.organizations.empty?
+        module_template.locations << Location.unscoped.all if module_template.locations.empty?
+      end
     end
   end
 end
