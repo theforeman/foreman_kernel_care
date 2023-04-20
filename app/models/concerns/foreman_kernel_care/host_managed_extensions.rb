9d95d26e07c78d21c27cb4c9e355efea9d280dfa
Maxim <maxmol27@gmail.com>
2023-04-20 15:46:25 +0200

    Fix #19 - Support for Debian/Ubuntu (#33)

--- app/models/concerns/foreman_kernel_care/host_managed_extensions.rb
+++ app/models/concerns/foreman_kernel_care/host_managed_extensions.rb
@@ -1,6 +1,5 @@
 module ForemanKernelCare
   module HostManagedExtensions
-
     def import_package_profile(simple_packages)
       if kernelcare?
         composer = ::JobInvocationComposer.for_feature(:kernel_version, self)
@@ -29,34 +28,76 @@ module ForemanKernelCare
       end
     end
 
-    def update_kernel_version(version, release)
-      packages = ::Katello::InstalledPackage.where('name LIKE ?', '%kernel%').where.not(name: 'kernelcare').to_a
-      delete_ids = []
-      simple_packages = packages.map do |p|
-        delete_ids << p.id
-        ::Katello::Pulp::SimplePackage.new({
-                                             arch: p.arch,
-                                             epoch: p.epoch,
-                                             version: version,
-                                             release: release,
-                                             name: p.name
-                                           })
+    def update_kernel_version(version, release, old_kernel_version)
+      old_version, old_release = old_kernel_version.split('-')
+
+      case operatingsystem.family
+      when 'Redhat'
+        packages = find_yum_kernel_packages(old_version, old_release)
+        return if packages.empty?
+
+        delete_yum_pkgs(packages)
+        create_new_yum_kernel_packages(packages, version, release)
+      when 'Debian'
+        old_version = "#{old_version}-#{old_release}"
+        new_version = "#{version}-#{release}"
+        packages = find_deb_kernel_packages(old_version)
+        return if packages.empty?
+
+        create_new_deb_kernel_packages(packages, new_version, old_version)
       end
-      found = import_package_profile_in_bulk(simple_packages)
-      sync_kernel_associations(found.map(&:id).uniq, delete_ids)
+    end
+
+    class ::Host::Managed::Jail < Safemode::Jail
+      allow :installed_debs
     end
 
     protected
 
-    def sync_kernel_associations(new_patched_kernel_ids, delete_ids)
+    def find_yum_kernel_packages(old_version, old_release)
+      installed_packages.select do |p|
+        p.name.include?('kernel') &&
+          p.name != 'kernelcare' &&
+          p.version == old_version &&
+          old_release.include?(p.release)
+      end
+    end
+
+    def delete_yum_pkgs(packages)
+      delete_ids = packages.map(&:id)
       ::Katello::Util::Support.active_record_retry do
         table_name = host_installed_packages.table_name
-
         queries = []
 
         if delete_ids.any?
           queries << "DELETE FROM #{table_name} WHERE host_id=#{id} AND installed_package_id IN (#{delete_ids.join(', ')})"
         end
+
+        queries.each do |query|
+          ::ActiveRecord::Base.connection.execute(query)
+        end
+      end
+    end
+
+    def create_new_yum_kernel_packages(packages, version, release)
+      new_kernels = packages.map do |p|
+        ::Katello::Pulp::SimplePackage.new({
+          arch: p.arch,
+          epoch: p.epoch,
+          version: version,
+          release: release,
+          name: p.name
+        })
+      end
+      found = import_package_profile_in_bulk(new_kernels)
+      sync_yum_kernel_associations(found.map(&:id).uniq)
+    end
+
+    def sync_yum_kernel_associations(new_patched_kernel_ids)
+      ::Katello::Util::Support.active_record_retry do
+        table_name = host_installed_packages.table_name
+
+        queries = []
 
         unless new_patched_kernel_ids.empty?
           inserts = new_patched_kernel_ids.map { |unit_id| "(#{unit_id.to_i}, #{id.to_i})" }
@@ -69,8 +110,38 @@ module ForemanKernelCare
       end
     end
 
+    def find_deb_kernel_packages(old_version)
+      installed_debs.select do |p|
+        p.name.include?('linux') && p.version.include?(old_version)
+      end
+    end
+
+    def create_new_deb_kernel_packages(packages, new_version, old_version)
+      old_kernel_ids = packages.map(&:id)
+      new_kernels = packages.map do |p|
+        name = if p.name.include?(old_version)
+                 p.name.sub(old_version, new_version)
+               else
+                 p.name
+               end
+        ::Katello::InstalledDeb.find_or_create_by(name: name, architecture: p.architecture, version: new_version)
+      end
+      return if new_kernels.empty? || old_kernel_ids.empty?
+
+      sync_deb_kernel_associations(new_kernels.map(&:id), old_kernel_ids)
+    end
+
+    def sync_deb_kernel_associations(new_patched_kernel_ids, old_kernel_ids)
+      new_installed_debs = installed_deb_ids.reject do |id|
+        old_kernel_ids.include?(id)
+      end
+      self.installed_deb_ids = new_installed_debs.concat(new_patched_kernel_ids)
+      save!
+    end
+
     def kernelcare?
-      !installed_packages.select { |package| package.name == 'kernelcare' }.empty?
+      !installed_packages.select { |package| package.name == 'kernelcare' }.empty? ||
+        !installed_debs.select { |package| package.name == 'kernelcare' }.empty?
     end
   end
 end
--- test/unit/host_managed_extensions_test.rb
+++ test/unit/host_managed_extensions_test.rb
@@ -9,24 +9,63 @@ module ForemanKernelCare
       feature = RemoteExecutionFeature.register('kernel_version', 'Kernel version')
       template = FactoryBot.create(:job_template, name: 'Kernel versionl')
       template.sync_feature(feature.label)
-
-      @host = FactoryBot.create(:host, :with_kernelcare)
-      package_json = {:name => "kernel", :version => "1", :release => "1.el7", :arch => "x86_64", :epoch => "1",
-                      :nvra => "kernel-1-1.el7.x86_64"}
-      @host.import_package_profile([::Katello::Pulp::SimplePackage.new(package_json)])
-      @nvra = 'kernel-1-1.el7.x86_64'
-      @host.reload
     end
 
     def test_update_kernel_version
+      host = FactoryBot.create(:host, :with_kernelcare,
+        :operatingsystem => FactoryBot.create(:operatingsystem,
+          :family => 'Redhat'))
+      package_json = { :name => 'kernel', :version => '1',
+                       :release => '1.el7', :arch => 'x86_64', :epoch => '1',
+                       :nvra => 'kernel-1-1.el7.x86_64' }
+      host.import_package_profile([::Katello::Pulp::SimplePackage.new(package_json)])
+      nvra = 'kernel-1-1.el7.x86_64'
+      host.reload
       version = '2'
       release = '2.el7'
-      @host.update_kernel_version(version, release)
-      kernel_package = @host.installed_packages.where(name: 'kernel').first
+      old_kernel_version = '1-1.el7.x86_64'
+      host.update_kernel_version(version, release, old_kernel_version)
+      kernel_package = host.installed_packages.where(name: 'kernel').first
 
-      assert_not_equal kernel_package.nvra, @nvra
+      assert_not_equal kernel_package.nvra, nvra
       assert_equal kernel_package.version, version
       assert_equal kernel_package.release, release
+    end
+
+    def test_update_kernel_version_debian
+      host = FactoryBot.create(:host, :debian_with_kernelcare,
+        :operatingsystem => FactoryBot.create(:operatingsystem,
+          :major => '10',
+          :release_name => 'rn10',
+          :family => 'Debian'))
+      kernels = [
+        { architecture: 'x86_64', version: '1-1', name: 'linux-image-generic' },
+        { architecture: 'x86_64', version: '1-1', name: 'linux-headers-generic ' },
+        { architecture: 'x86_64', version: '1-1', name: 'linux-generic' },
+        { architecture: 'x86_64', version: '1-1', name: 'linux-signed-generic' },
+        { architecture: 'x86_64', version: '1-1', name: 'linux-image-1-1-generic' }
+      ]
+      kernel_deb_ids = kernels.map do |kernel|
+        ::Katello::InstalledDeb.find_or_create_by(name: kernel[:name],
+                                                  architecture: kernel[:architecture],
+                                                  version: kernel[:version]).id
+      end
+      kernelcare_id = host.installed_deb_ids.first
+      installed_deb_ids = kernel_deb_ids << kernelcare_id
+      host.installed_deb_ids = installed_deb_ids
+      host.save!
+
+      version = '2'
+      release = '2'
+      old_kernel_version = '1-1-generic'
+      host.update_kernel_version(version, release, old_kernel_version)
+      debian_kernel_packages = host.installed_debs.select do |p|
+        p.name.include?('generic')
+      end
+      debian_kernel_packages.each do |p|
+        assert_not_equal p.name, 'linux-image-1-1-generic'
+        assert_equal p.version, "#{version}-#{release}"
+      end
     end
   end
 
