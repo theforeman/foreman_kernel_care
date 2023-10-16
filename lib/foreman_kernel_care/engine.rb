c7bd4d544af6a94a9d94cacf7ef847cbf28ce8c4
Bernhard Suttner <sbernhard@users.noreply.github.com>
2023-10-16 14:28:25 +0200

    Handle new debian profiles upload (#58)

--- lib/foreman_kernel_care/engine.rb
+++ lib/foreman_kernel_care/engine.rb
@@ -8,6 +8,7 @@ module ForemanKernelCare
     engine_name 'foreman_kernel_care'
 
     config.autoload_paths += Dir["#{config.root}/app/models/concerns"]
+    config.autoload_paths += Dir["#{config.root}/app/services/concerns"]
 
     initializer 'foreman_kernel_care.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_kernel_care do
@@ -19,6 +20,7 @@ module ForemanKernelCare
     # make sure the plugin is initialized before katello loads the host extensions
     initializer 'foreman_kernel_care.load_kernelcare_override', :before => :finisher_hook, :after => ['katello.register_plugin', 'foreman_tasks.register_plugin'] do
       ::Katello::Concerns::HostManagedExtensions.prepend ForemanKernelCare::HostManagedExtensions
+      ::Katello::Host::ProfilesUploader.prepend ForemanKernelCare::ProfilesUploader
       ::ForemanTasks::Api::TasksController.prepend ForemanKernelCare::ForemanTasks
     end
 
